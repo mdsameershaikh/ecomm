e5a0ca89347f1a4de70e24a41f05d55a3729c781
Sameer Shaikh <shaikhs975@gmail.com>
2023-12-19 19:23:56 +0530

    added product card

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="app">
     <page-head />
-
+    <div id="product-crousel">
+      crousel
+    </div>
+    <div id="product-grid">
+      <product />
+      <product />
+      <product />
+      <product />
+      <product />
+      <product />
+      <product />
+    </div>
+    <div id="footer-section">
+      Footer section
+    </div>
   </div>
 </template>
 
 <script>
 import page_head from './components/page-head.vue'
+import product from './components/products/product.vue'
 
 export default {
   name: 'App',
   components: {
-    'page-head': page_head
+    'page-head': page_head,
+    product
+
   }
 }
 </script>
@@ -24,5 +41,32 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+#product-crousel {
+  height: 300px;
+  width: 60%;
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+}
+
+#product-grid {
+  height: 600px;
+  width: 60%;
+  border: 1px solid black;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  gap: 10px
+}
+
+#footer-section {
+  height: 200px;
+  width: 100%;
+  border: 1px solid black;
 }
 </style>
